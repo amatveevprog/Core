7a75aaceb3e53cc0627dfcdc4cd8ff084a22955e
amatveevprog <alexey.matveev1@rambler.ru>
2016-09-21 16:41:10 +0300

    response update

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,17 @@ var asyncRouter = require('../async_core');
 var HttpError = require('../errors').HttpError;
 exports.get = function(req,res,next){
     //проверка на корректный урл
-    asyncRouter.executeFunction(req.url,function(err,result){
-       if(err)
-       {
-           return next(err);
-       }
-       else
-       {
-           res.end("result: "+JSON.stringify(result));
-       }
+    asyncRouter.executeFunction(req.url,res,function(err,result){
+        if(err)
+        {
+            return next(err);
+        }
+        else
+        {
+            console.log("executed!!!");
+
+            //res.end("result: "+JSON.stringify(result));
+        }
     });
     //console.log(req.url);
     //res.end(JSON.stringify(req.url));
