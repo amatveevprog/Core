5a61ccbf8818114a14e41581c4863f9c3168d4c5
alexander.bondarik <avbondarik@gmail.com>
2016-09-22 22:22:44 +0300

    123

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,7 +1,7 @@
-console.log('ololol now there will be the error:');
+
 var config = require('../config/config');
 var mongoose = require('mongoose');
 
 
-//mongoose.connect(config.mongoose.uri,config.mongoose.options);
+mongoose.connect(config.mongoose.uri,config.mongoose.options);
 module.exports = mongoose;
