a9c917ac07cfc8c6b34f6bfe0e9d67aded6cd0f7
alexander.bondarik <avbondarik@gmail.com>
2016-09-22 22:19:33 +0300

    123

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -3,5 +3,5 @@ var config = require('../config/config');
 var mongoose = require('mongoose');
 
 
-mongoose.connect(config.mongoose.uri,config.mongoose.options);
+//mongoose.connect(config.mongoose.uri,config.mongoose.options);
 module.exports = mongoose;
