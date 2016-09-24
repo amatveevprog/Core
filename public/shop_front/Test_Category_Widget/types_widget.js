09ec90061adfb107b53874c30c9d657d70305fa7
alexander.bondarik <avbondarik@gmail.com>
2016-09-24 14:59:57 +0300

    TestCommit

---
+++ public/shop_front/Test_Category_Widget/types_widget.js
@@ -0,0 +1,28 @@
+/**
+ * Created by alexander.bondarik on 24.09.2016.
+ */
+// [=================== Categories Widget =======================]
+
+var types_menu = function()
+{
+    this.target_div = '';
+    //this.categories_object = {};
+
+    this.init = function(types_menu_object,categories_object)
+    {
+      var types_div = document.createElement('div');
+      for(var type in types_menu_object.types_data)
+      {
+         var button = document.createElement('button');
+         button.innerText = types_menu_object.types_data[type].groups_label;
+         button.id =  types_menu_object.types_data[type].groups_label;
+         types_div.appendChild();
+      }
+      document.getElementById(types_menu_object.target_div).appendChild(types_div);
+
+    }
+}
+
+types_menu_object = new types_menu();
+types_menu_object.target_div = 'types_area';
+types_menu_object.init(types_menu_object,cart_widget.categories_data);
