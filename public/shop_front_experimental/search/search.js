ec66a74dfe20aa4b41c71615f430d4f37e0477bc
amatveevproq <amatveev.prog@gmail.com>
2016-10-25 01:26:35 +0300

    updated removing cart items when pressed "No" to update session on a client!!!

---
+++ public/shop_front_experimental/search/search.js
@@ -0,0 +1,10 @@
+//основная функция назначения клавиш, где при вводе будет осуществляться подбор ключевых слов
+//создает DOM-узлы(1 или более outputElem) и прикрепляет их к parentNode
+function reqisterOnKeyboard(parentNode,outputElem)
+{
+
+}
+function searchOnKeywords(args)
+{
+
+}
